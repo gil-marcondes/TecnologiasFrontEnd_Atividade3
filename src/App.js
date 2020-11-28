dc9c007eecb8c59e67cb0ab5b706e33709b86823
gil-marcondes <73506561+gil-marcondes@users.noreply.github.com>
2020-11-28 14:41:37 -0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Tamanho from './Tamanho';
+import './App.css';
+
+class App extends Component{
+  render(){
+  return (
+  <div className="App">
+  <h1>Tecnologias Front-end: Atividade3</h1>
+        <Tamanho/>
+		
+      </div>
+	  );
+}
+}
+
+export default App;
